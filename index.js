7e8838da52a96f43f1ead251c9dcba7ae7885035
Imane Belaid <belaidimane@gmail.com>
2023-08-13 15:32:13 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Remove main element
+let main = document.getElementById("main");
+main.remove();
+
+//Create newHeader => h1 element
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.textContent = "Eman is the champion";
+
+
